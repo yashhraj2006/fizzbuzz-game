1ace2780a059446f71c32ae1e82b34ea9b0c7e32
yashhraj2006 <109662596+yashhraj2006@users.noreply.github.com>
2022-07-20 15:33:59 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+a=float(input("enter your marks "))
+if (a>=90):
+    print("you will get iphone")
+elif(a>=70):
+    print("you will get a laptop")
+elif(a>=50):
+    print("you will get a treat")
+else:
+    print("YOU NEED TO IMPROVE")        
